7e1fdd6d6cb22f65e5877af2e7b6796058818d98
Suganya Anbazhagan <suganyamuthu9560@gmail.com>
2025-02-07 00:08:13 -0700

    reduc open menu state changes

--- src/redux/appSlice.js
+++ src/redux/appSlice.js
@@ -8,7 +8,7 @@ const appSlice = createSlice({
   },
   reducers: {
     toggleMenu: (state) => {
-      state.isMenuOpen = true;
+      state.isMenuOpen = !state.isMenuOpen;
     },
     closeMenu: (state) => {
       state.isMenuOpen = false;
